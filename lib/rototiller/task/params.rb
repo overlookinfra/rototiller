8161c71101900107ab92a61981f82d99bb8cba45
Eric Thompson <eric.thompson@puppetlabs.com>
2018-05-03 15:44:19 -0700

    (maint) bump up code coverage
    * add a comment on SLV-116 to fix tech-debt in `RototillerParam`
    * add some tests around `#parent_name=`
    * add some docs around `RototillerParam`'s methods

--- lib/rototiller/task/params.rb
+++ lib/rototiller/task/params.rb
@@ -21,6 +21,13 @@ module Rototiller
         return ''
       end
 
+      # FIXME: (SLV-116) we shouldn't have env_var specific stuff in here
+      #   we don't use this in other params, but if we did it would break them
+      #   this breaks encapsulation a bit
+      # Record parent parameter's name so we can message appropriately
+      # @param [String] name parent's parameter instance name
+      # @api public
+      # @example param.parent_name = otherparam.name
       def parent_name=(name)
         name.each_char do |char|
           message = "You have defined an environment variable with an illegal character: #{char}"
@@ -29,6 +36,10 @@ module Rototiller
         @parent_name = name
       end
 
+      # Record parent param's message so we can message appropriately
+      # @param [String] name parent parameter's message
+      # @api public
+      # @example param.parent_message = otherparam.message
       def parent_message=(message)
         @parent_message = message
       end

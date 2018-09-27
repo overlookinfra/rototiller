f79b4eac25d1311edc95c8d81592ede468627ca7
Eric Thompson <eric.thompson@puppetlabs.com>
2018-09-27 15:56:48 -0700

    (maint) fixup previous errorneous conflict resolution

--- spec/rototiller/task/params_spec.rb
+++ spec/rototiller/task/params_spec.rb
@@ -9,20 +9,6 @@ module Rototiller
       it "fails when trying to set a message on the root class instance" do
         expect { described_class.new.message = "blah" }.to raise_error(NoMethodError)
       end
-      context "#parent_name=" do
-        let(:param) { described_class.new }
-        it "sets @parent_name" do
-          param.parent_name = "superkoolname"
-          expect param.parent_name.to eq("superkoolname")
-        end
-        it "protects against illegal env_vars" do
-          # raise ArgumentError.new(message) unless char =~ /[a-zA-Z]|\d|_/
-          # no hyphens
-          expect { param.parent_name = "-" }.to raise_error(ArgumentError)
-          # no special chars
-          expect { param.parent_name = "%" }.to raise_error(ArgumentError)
-        end
-      end
     end
   end
 end
